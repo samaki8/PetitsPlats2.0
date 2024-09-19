765b81b279dc3b5422370c612d14aa737d0bc88c
samaki8 <samira08@gmail.com>
2024-09-19 17:20:44 +0200

    Finalisation site

--- scripts/models/RecipeCard.js
+++ scripts/models/RecipeCard.js
@@ -1,20 +1,32 @@
 // RecipeCard.js
+// RecipeCard.js
 export function createRecipeCard(recipe) {
     return `
-        <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-                <img src="./data/JSON recipes/${recipe.image}" class="card-img-top" alt="${recipe.name}">
-                <div class="card-body">
+        <div class="col">
+            <div class="card h-100 shadow-sm" >
+                <div class="position-relative">
+                    <img src="./data/JSON recipes/${recipe.image}" class="card-img-top" alt="${recipe.name}">
+                    <span class="badge bg-warning position-absolute top-0 end-0 m-2">${recipe.time} min</span>
+                </div>
+                <div class="card-body px-4">
                     <h5 class="card-title">${recipe.name}</h5>
+                    <h6 class="card-tt mt-4 mb-2">RECETTE</h6>
                     <p class="card-text">${recipe.description}</p>
-                    <ul class="list-group list-group-flush">
+                    <h6 class="card-tt mt-5">INGRÉDIENTS</h6>
+                    <div class="row">
                         ${recipe.ingredients.map(ingredient => `
-                            <li class="list-group-item">${ingredient.ingredient}: ${ingredient.quantity || ''} ${ingredient.unit || ''}</li>
+                            <div class="col-6">
+                                <li class="list-group-item">
+                                    ${ingredient.ingredient}<br>
+                                    <small>${ingredient.quantity || ''} ${ingredient.unit || ''}</small>
+                                </li>
+                            </div>
                         `).join('')}
-                    </ul>
-                    <p class="card-text"><small class="text-muted">Temps de préparation: ${recipe.time} minutes</small></p>
+                    </div>
+                  <!--  <p class="card-text"><small class="text-muted">Temps de préparation: ${recipe.time} minutes</small></p> -->
                 </div>
             </div>
         </div>
     `;
 }
+
